dcdc8146645550f354221ff8ad5e0663aca5dae8
tumipetteri <tuomo.pelkonen@gmail.com>
2014-01-23 16:09:48 +0200

    Added missing Visual studio files

---
+++ precompile.h
@@ -0,0 +1,25 @@
+#ifndef __INCLUDED_PRECOMPILE_H
+#define __INCLUDED_PRECOMPILE_H
+
+#include <string>
+#include <list>
+#include <iostream>
+#include <fstream>
+#include <math.h>
+
+#ifdef __MACOS
+#include <SDL2/SDL_opengl.h>
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#endif
+
+#ifdef WIN32
+#include <SDL_opengl.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#endif
+
+
+
+
+#endif
