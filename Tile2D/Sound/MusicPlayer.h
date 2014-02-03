24bcaeec2d15fbabe9c759031cfde34d3475d64c
Miika Pelkonen <pelkonen.miika@gmail.com>
2014-02-03 09:51:55 +0200

    some minor fixes

--- Tile2D/Sound/MusicPlayer.h
+++ Tile2D/Sound/MusicPlayer.h
@@ -1,18 +1,18 @@
-// This file is part of JustFoolingAround.
+// This file is part of SpaceGame.
 // Copyright (C) 2014 Miika Pelkonen
 //
-// JustFoolingAround is free software: you can redistribute it and/or modify
+// SpaceGame is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// JustFoolingAround is distributed in the hope that it will be useful,
+// SpaceGame is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with JustFoolingAround.  If not, see <http://www.gnu.org/licenses/>.
+// along with SpaceGame.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef __MusicPlayer_H_
 #define __MusicPlayer_H_
