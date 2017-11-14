80b4a0512141edd58e34c60b2d4262fc2d90f051
Miika Pelkonen <pelkonen.miika@gmail.com>
2017-11-14 16:36:25 +0200

    GPL comments

--- Tile2D/Core/SceneManager.h
+++ Tile2D/Core/SceneManager.h
@@ -1,6 +1,18 @@
+// This file is part of SpaceGame.
+// Copyright (C) 2017 Miika Pelkonen
 //
-// Created by Miika Pelkonen on 10/11/2017.
+// SpaceGame is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
+// SpaceGame is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with SpaceGame.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef __SCENEMANAGER_H
 #define __SCENEMANAGER_H
