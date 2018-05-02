06f63c316acac317ce64313134bf6f2a55ab5901
Miika Pelkonen <pelkonen.miika@gmail.com>
2018-05-02 06:33:03 +0300

    minor change: ambient light to zero + spaceship sprite size modification

--- Game/SceneInGame/SceneInGame.cpp
+++ Game/SceneInGame/SceneInGame.cpp
@@ -37,6 +37,7 @@
 void SceneInGame::init() {
     // Scene setup: tile map, physics, light system
     Tile2D::tileMap().load("data/maps/map.bmp", "data/maps/tileset.json");
+    Tile2D::lightSystem().setAmbientLight(0.0f);
     Tile2D::physicsWorld().setAirDensity(0.2f);
     Tile2D::physicsWorld().setGForce({0, 100.0f});
 
